fb0be34fd2e05ed58ae16ea07e80533b6818b781
Faiter119 <olavh96@gmail.com>
2019-04-11 16:51:40 +0200

    Model at >90% accuracy

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,33 @@
 import src.model.Loader as Loader
 import src.model.Prepare as Prepare
 import src.model.Labels as Labels
+import src.model.Train as Train
 
 import numpy as np
 
 if __name__ == '__main__':
     data, labels = Loader.load()
-    labels = [Labels.from_char(l) for l in labels]
+    labels = np.array([Labels.from_char(l) for l in labels])
 
-    normalized_data: np.ndarray = Prepare.normalize(data)
+    data, labels = Prepare.shuffle(data, labels)
+    data: np.ndarray = Prepare.normalize(data)
+    train_data, test_data, train_labels, test_labels = Prepare.split(data, labels, 0.2)
+    train_data = Prepare.expand_dims(train_data)
+    test_data = Prepare.expand_dims(test_data)
 
-    print(normalized_data.shape)
-    print(Prepare.flatten(normalized_data).shape)
+    model = Train.build_model((data.shape[1], data.shape[2], 1), output_len=Labels.from_char('z') + 1)
 
+    model = Train.train(train_data, train_labels, model)
+
+    loss, accuracy = Train.evaluate(model, train_data, train_labels)
+    print("Model Loss", loss)
+    print("Model Accuracy", accuracy)
+
+    loss, accuracy = Train.evaluate(model, test_data, test_labels)
+    print("Model Loss", loss)
+    print("Model Accuracy", accuracy)
+
+    for x, y in zip(test_data[:10], labels[:10]):
+        predict = Train.predict(model, x.reshape(1, 20, 20, 1))
+
+        print(predict, y)
--- src/model/Prepare.py
+++ src/model/Prepare.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.utils import shuffle as skshuffle
 
 
 def normalize(data: np.ndarray) -> np.ndarray:
@@ -12,3 +13,25 @@ def flatten(data: np.ndarray) -> np.ndarray:
     flattened = data.reshape((s[0], s[1] * s[2]))
 
     return flattened
+
+
+def split(data: np.ndarray, labels, percent_test) -> (np.ndarray, np.ndarray, [int], [int]):
+    l = data.shape[0]
+    i_test = int(l * percent_test)
+    test, train = np.split(data, [i_test])
+
+    train_labels = labels[i_test:]
+    test_labels = labels[:i_test]
+    train_labels = np.array(train_labels)
+    test_labels = np.array(test_labels)
+
+    return train, test, train_labels, test_labels
+
+
+def expand_dims(data: np.ndarray) -> np.ndarray:
+    return np.expand_dims(data, len(data.shape))
+
+
+def shuffle(x: np.ndarray, y: np.ndarray):
+    x, y = skshuffle(x, y)
+    return x, y
